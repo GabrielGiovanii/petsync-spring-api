95f908492796afa719ad316bb4829340564cae13
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-08-18 17:40:21 -0300

    Creating the initial structure of the project

---
+++ src/main/java/com/petsync_spring_api/petsync_spring_api/PetsyncSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.petsync_spring_api.petsync_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsyncSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsyncSpringApiApplication.class, args);
+	}
+
+}
