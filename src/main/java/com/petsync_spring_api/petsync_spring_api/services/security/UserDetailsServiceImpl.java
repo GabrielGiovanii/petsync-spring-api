c4a85b1bbbd72335b4a98312aad95765e3f38b6f
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-09-06 11:53:59 -0300

    Creating implementation of UserDetails and UserDetailsService that will be responsible for authenticating and creating an Authentication object

---
+++ src/main/java/com/petsync_spring_api/petsync_spring_api/services/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.petsync_spring_api.petsync_spring_api.services.security;
+
+import com.petsync_spring_api.petsync_spring_api.entities.User;
+import com.petsync_spring_api.petsync_spring_api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String cpf) throws UsernameNotFoundException {
+        Optional<User> userOptional = Optional.of(userService.selectByCode(cpf));
+
+        return  userOptional.map(UserAuth::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with cpf: " + cpf));
+    }
+}
