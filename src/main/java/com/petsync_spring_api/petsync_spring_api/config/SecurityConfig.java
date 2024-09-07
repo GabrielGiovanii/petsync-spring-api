ecfacb336d99abbd0debad1a217fb7cee22581de
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-09-07 15:55:18 -0300

    Add CORS configuration

--- src/main/java/com/petsync_spring_api/petsync_spring_api/config/SecurityConfig.java
+++ src/main/java/com/petsync_spring_api/petsync_spring_api/config/SecurityConfig.java
@@ -21,9 +21,13 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -45,7 +49,16 @@ public class SecurityConfig {
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .oauth2ResourceServer(config -> config.jwt(jwt -> jwt.decoder(jwtDecoder())));
+                .oauth2ResourceServer(config -> config.jwt(jwt -> jwt.decoder(jwtDecoder())))
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            CorsConfiguration corsConfig = new CorsConfiguration();
+                            corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+                            corsConfig.setAllowedMethods(List.of("*"));
+                            corsConfig.setAllowedHeaders(List.of("*"));
+                            return corsConfig;
+                        })
+                );
 
         return  http.build();
     }
