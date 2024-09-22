dca17a79e81a611c50710d7a36a20598078eb72d
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-09-21 21:32:34 -0300

    Adding Repository annotation to the missing created Repositories

--- src/main/java/com/petsync_spring_api/petsync_spring_api/repositories/RoleRepository.java
+++ src/main/java/com/petsync_spring_api/petsync_spring_api/repositories/RoleRepository.java
@@ -2,6 +2,8 @@ package com.petsync_spring_api.petsync_spring_api.repositories;
 
 import com.petsync_spring_api.petsync_spring_api.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
--- src/main/java/com/petsync_spring_api/petsync_spring_api/repositories/UserPhoneRepository.java
+++ src/main/java/com/petsync_spring_api/petsync_spring_api/repositories/UserPhoneRepository.java
@@ -2,6 +2,8 @@ package com.petsync_spring_api.petsync_spring_api.repositories;
 
 import com.petsync_spring_api.petsync_spring_api.entities.UserPhone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserPhoneRepository extends JpaRepository<UserPhone, Integer> {
 }
